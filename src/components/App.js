f2c001f417432fb4c439c548ec523b5f174fdc7c
Ermondel <ermondel@gmail.com>
2020-04-02 20:31:48 +0300

    feat: add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
 
-const App = () => <div>run app</div>;
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/show" exact component={StreamShow} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
