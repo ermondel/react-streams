504d3e9ed0d648dd13c4bfd108b4cae5ceda03cf
Ermondel <ermondel@gmail.com>
2020-04-06 22:27:05 +0300

    feat: actions and reducer for CRUD operations on streams

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,33 @@
+import {
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from '../actions/types';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+    case CREATE_STREAM:
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+
+    case DELETE_STREAM:
+      const updatedState = { ...state };
+      if (updatedState[action.payload]) {
+        delete updatedState[action.payload];
+      }
+      return updatedState;
+
+    case FETCH_STREAMS:
+      const nextState = { ...state };
+      action.payload.forEach((obj) => {
+        nextState[obj.id] = obj;
+      });
+      return nextState;
+
+    default:
+      return state;
+  }
+};
