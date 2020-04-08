76d08f6ef957335fcaad1e56b7d267477a7ecf8b
Ermondel <ermondel@gmail.com>
2020-04-08 10:00:24 +0300

    feat: change navigation in routers

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamShow from './streams/StreamShow';
 import StreamDelete from './streams/StreamDelete';
 import Header from './Header';
+import history from '../history';
 import './App.css';
 
 const App = () => (
   <div className="ui container app">
-    <BrowserRouter>
+    <Router history={history}>
       <div>
         <Header />
         <Route path="/" exact component={StreamList} />
@@ -19,7 +20,7 @@ const App = () => (
         <Route path="/streams/show" exact component={StreamShow} />
         <Route path="/streams/delete" exact component={StreamDelete} />
       </div>
-    </BrowserRouter>
+    </Router>
   </div>
 );
 
