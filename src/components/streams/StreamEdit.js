a8cb624574f0d71e58a6cf8b4f0bde8cab4245eb
Ermondel <ermondel@gmail.com>
2020-04-08 16:39:31 +0300

    feat: add stream editing boilerplate

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends Component {
+  componentDidMount() {
+    if (!this.props.stream) {
+      this.props.fetchStream(this.props.streamId);
+    }
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return <div>{this.props.stream.title}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const streamId = ownProps.match.params.id;
+
+  return {
+    streamId,
+    stream: state.streams[streamId],
+  };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
